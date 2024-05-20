79813318f040ad5e02f3c97a97bd73d77310c359
blastka <svetuk505@mail.ru>
2024-05-20 13:58:14 +0300

    tests

--- app/src/test/java/eu/maxkim/boredombuster/activity/ui/newactivity/CoroutinesTestRules.kt
+++ app/src/test/java/eu/maxkim/boredombuster/activity/ui/newactivity/CoroutinesTestRules.kt
@@ -10,7 +10,7 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class CoroutinesTestRules(private val testDispatcher: TestDispatcher = StandardTestDispatcher()):
+class CoroutinesTestRules(val testDispatcher: TestDispatcher = StandardTestDispatcher()):
     TestWatcher() {
     override fun starting(description: Description?) {
         Dispatchers.setMain(testDispatcher)
--- app/src/test/java/eu/maxkim/boredombuster/activity/ui/newactivity/NewActivityViewModelTest.kt
+++ app/src/test/java/eu/maxkim/boredombuster/activity/ui/newactivity/NewActivityViewModelTest.kt
@@ -16,39 +16,144 @@ import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.lang.Exception
 import java.lang.RuntimeException
+
 @OptIn(ExperimentalCoroutinesApi::class)
-class NewActivityViewModelTest{
+class NewActivityViewModelTest {
 
     @get:Rule
     val coroutinesTestRules = CoroutinesTestRules()
+
     @Test
-    fun `creating a viewmodel exposes loading ui state`() {
-        // Arrange
+    fun `после создания viewModel начальное значение uiState равно NewActivityUiState Loading`() {
         val viewModel = NewActivityViewModel(
             FakeGetRandomActivity(),
             FakeSaveActivity(),
             FakeDeleteActivity(),
             FakeIsActivitySaved()
         )
-         assert(viewModel.uiState.value is NewActivityUiState.Loading)
+        assert(viewModel.uiState.value is NewActivityUiState.Loading)
     }
 
+    @Test
+    fun `после создания экземпляра NewActivityViewModel состояние ui равно NewActivityUiState Success`() {
+        val viewModel = NewActivityViewModel(
+            FakeGetRandomActivity(),
+            FakeSaveActivity(),
+            FakeDeleteActivity(),
+            FakeIsActivitySaved()
+        )
+        val expectedUiState = NewActivityUiState.Success(activity1, false)
+        coroutinesTestRules.testDispatcher.scheduler.runCurrent() //завершение всех сопрограмм
+        val currentState = viewModel.uiState.value
+        assertEquals(expectedUiState, currentState)
+    }
+
+    @Test
+    fun `после создания viewModel состояние ui корректно обновляется до NewActivityUiState Error`() {
+        val viewModel = NewActivityViewModel(
+            FakeGetRandomActivity(false),
+            FakeSaveActivity(),
+            FakeDeleteActivity(),
+            FakeIsActivitySaved()
+        )
+        coroutinesTestRules.testDispatcher.scheduler.runCurrent()//завершение всех сопрограмм
+
+        val currentState = viewModel.uiState.value
+        assert(currentState is NewActivityUiState.Error)
+    }
+
+    @Test
+    fun `если действие уже сохранено, для isFavorite флага состояния пользовательского интерфейса установлено значение true`() {
+        // Arrange
+        val viewModel = NewActivityViewModel(
+            FakeGetRandomActivity(), // our fake will return an error
+            FakeSaveActivity(),
+            FakeDeleteActivity(),
+            FakeIsActivitySaved(isActivitySaved = true)
+        )
+
+        val expectedUiState = NewActivityUiState.Success(activity1, true)
+
+        // Act
+        coroutinesTestRules.testDispatcher.scheduler.runCurrent()//завершение всех сопрограмм
+
+        // Assert
+        val actualState = viewModel.uiState.value
+        assertEquals(actualState, expectedUiState)
+    }
+
+    @Test
+    fun `calling loadNewActivity() updates ui state with a new activity`() {
+        // Arrange
+        val fakeGetRandomActivity = FakeGetRandomActivity()
+        val viewModel = NewActivityViewModel(
+            fakeGetRandomActivity,
+            FakeSaveActivity(),
+            FakeDeleteActivity(),
+            FakeIsActivitySaved()
+        )
+        coroutinesTestRules.testDispatcher.scheduler.runCurrent()//завершение всех сопрограмм
+
+        val expectedResult = NewActivityUiState.Success(activity2, false)
+        fakeGetRandomActivity.activity = activity2
+
+        viewModel.loadNewActivity()
+        coroutinesTestRules.testDispatcher.scheduler.runCurrent()//завершение всех сопрограмм
+
+        val actualResult = viewModel.uiState.value
+        assertEquals(actualResult, expectedResult)
+    }
+
+    @Test
+    fun `calling setIsFavorite(true) triggers SaveActivity use case`() {
+        val fakeSaveActivity = FakeSaveActivity()
+        val viewModel = NewActivityViewModel(
+            FakeGetRandomActivity(),
+            fakeSaveActivity,
+            FakeDeleteActivity(),
+            FakeIsActivitySaved()
+        )
+
+        viewModel.setIsFavorite(activity1, true)
+        coroutinesTestRules.testDispatcher.scheduler.runCurrent()//завершение всех сопрограмм
+
+        assert(fakeSaveActivity.wasCalled)
+    }
+
+    @Test
+    fun `calling setIsFavorite(false) triggers DeleteActivity use case`() {
+        // Arrange
+        val fakeDeleteActivity = FakeDeleteActivity()
+        val viewModel = NewActivityViewModel(
+            FakeGetRandomActivity(),
+            FakeSaveActivity(),
+            fakeDeleteActivity,
+            FakeIsActivitySaved()
+        )
+
+        viewModel.setIsFavorite(activity1, false)
+
+        coroutinesTestRules.testDispatcher.scheduler.runCurrent()//завершение всех сопрограмм
+
+        assert(fakeDeleteActivity.wasCalled)
+    }
 }
 
-class FakeGetRandomActivity(): GetRandomActivity{
+class FakeGetRandomActivity(val isSuccessful: Boolean = true) : GetRandomActivity {
 
-    val isSuccessful = true
+    var activity: Activity? = null
     override suspend fun invoke(): Result<Activity> {
         return if (isSuccessful)
-            Result.Success(activity1)
+            Result.Success(activity ?: activity1)
         else
             Result.Error(RuntimeException("error"))
     }
 }
 
-class FakeIsActivitySaved(): IsActivitySaved {
-    val isActivitySaved = false
+class FakeIsActivitySaved(val isActivitySaved: Boolean = false) : IsActivitySaved {
+
     override suspend fun invoke(key: String): Boolean {
         return isActivitySaved
     }
@@ -57,15 +162,19 @@ class FakeIsActivitySaved(): IsActivitySaved {
 
 class FakeSaveActivity : SaveActivity {
 
+    var wasCalled = false
+        private set
+
     override suspend fun invoke(activity: Activity) {
-        TODO("Not yet implemented")
+        wasCalled = true
     }
 }
 
 class FakeDeleteActivity : DeleteActivity {
-
+    var wasCalled = false
+        private set
     override suspend fun invoke(activity: Activity) {
-        TODO("Not yet implemented")
+        wasCalled = true
     }
 }
 
